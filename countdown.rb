441a4a42a6cf4a4a00f584693e3120a8f1b523f4
Taylor Mills <taylormills24@yahoo.com>
2019-03-02 17:08:17 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
-#write your code here
+countdown = 10
 
-def countdown
+while countdown < 10
+  countdown += 1
+  puts "Happy New Year!"
+  counter += 1
+  puts "#{number} second(s)!"
+end
+
+def countdown_with_sleep
+10.times do
+  puts "Happy New Year!"
+end
 end
